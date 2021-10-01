5d428d7cafb4f5401515e0ef6697a9984a92bfe1
Shivam Mishra <58064491+Shiva-78@users.noreply.github.com>
2021-10-01 15:07:18 -0500

    refactoring

--- src/main/java/com/hrksolutions/authservice/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.hrksolutions.authservice.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.hrksolutions.authservice.entities.UserEntity;
-
-public interface UserRepository extends CrudRepository<UserEntity, Long> {
-	UserEntity findByUsername(String username);
-}
